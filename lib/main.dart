de59a5111e7c42aba37a88a9e8c1fcd7b0d6c6fe
EliorCohen <elior123456123123@gmail.com>
2020-03-10 11:48:50 +0200

    Initial update

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfirebase/realtime_database.dart';
+
+void main() => runApp(RealtimeDatabaseApp());
---
+++ lib/realtime_database.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+
+class RealtimeDatabaseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Realtime Database',
+      theme: ThemeData.light(),
+      home: RealtimeDatabase(),
+    );
+  }
+}
+
+class RealtimeDatabase extends StatefulWidget {
+  @override
+  RealtimeDatabaseState createState() => RealtimeDatabaseState();
+}
+
+class RealtimeDatabaseState extends State<RealtimeDatabase> {
+  List<Item> items = List();
+  Item item;
+  DatabaseReference itemRef;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    item = Item("", "");
+    final FirebaseDatabase database = FirebaseDatabase.instance;
+    itemRef = database.reference().child('items');
+    itemRef.onChildAdded.listen(_onEntryAdded);
+    itemRef.onChildChanged.listen(_onEntryChanged);
+  }
+
+  _onEntryAdded(Event event) {
+    setState(() {
+      items.add(Item.fromSnapshot(event.snapshot));
+    });
+  }
+
+  _onEntryChanged(Event event) {
+    var old = items.singleWhere((entry) {
+      return entry.key == event.snapshot.key;
+    });
+    setState(() {
+      items[items.indexOf(old)] = Item.fromSnapshot(event.snapshot);
+    });
+  }
+
+  void handleSubmit() {
+    final FormState form = formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      form.reset();
+      itemRef.push().set(item.toJson());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Realtime Database'),
+      ),
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 0,
+            child: Center(
+              child: Form(
+                key: formKey,
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("Realtime Database",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold)),
+                    ListTile(
+                      leading: Icon(Icons.info),
+                      title: TextFormField(
+                        initialValue: "",
+                        onSaved: (val) => item.title = val,
+                        validator: (val) => val == "" ? val : null,
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.info),
+                      title: TextFormField(
+                        initialValue: '',
+                        onSaved: (val) => item.body = val,
+                        validator: (val) => val == "" ? val : null,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () {
+                        handleSubmit();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            child: FirebaseAnimatedList(
+              query: itemRef,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                  Animation<double> animation, int index) {
+                return new ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text(items[index].title),
+                  subtitle: Text(items[index].body),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Item {
+  String key;
+  String title;
+  String body;
+
+  Item(this.title, this.body);
+
+  Item.fromSnapshot(DataSnapshot snapshot)
+      : key = snapshot.key,
+        title = snapshot.value["title"],
+        body = snapshot.value["body"];
+
+  toJson() {
+    return {
+      "title": title,
+      "body": body,
+    };
+  }
+}
